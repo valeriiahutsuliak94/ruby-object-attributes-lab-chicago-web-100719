16052e42c7f5a5bf152679ba5ab4f8ee552dc0b2
Valeria <valeria.gutsuliak@gmail.com>
2019-10-08 19:58:39 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person 
+    def name=(name)
+        @name = name
+    end 
+    def name 
+        @name
+        def job=(job)
+            @job = job
+        end
+        def job 
+            @job 
+        end
+    end
